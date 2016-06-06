8d0102e7ab36bb35f1feb4bb4ce9ff1096bd1198
Manoj Krishnan <manoj.k@freshdesk.com>
2016-06-06 10:27:53 +0530

    Git Ignore updated, Gruntfile updated, Dependencies updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,16 +23,6 @@ module.exports = function(grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    },
-
     //AWS Access Keys
     aws: grunt.file.readJSON('aws.json'),
 
@@ -71,15 +61,13 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-aws-s3');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean','aws_s3_redirects:set_redirects']);
+  grunt.registerTask('test', ['aws_s3_redirects:set_redirects']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['jshint']);
 
 };
