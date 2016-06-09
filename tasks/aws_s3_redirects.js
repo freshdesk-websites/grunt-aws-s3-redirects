648067cf88cf6ebe68f7bd5eea09bf69bac59874
Manoj Krishnan <manoj.k@freshdesk.com>
2016-06-09 11:23:49 +0530

    AWS Website Redirect Location Fix

--- tasks/aws_s3_redirects.js
+++ tasks/aws_s3_redirects.js
@@ -23,6 +23,12 @@ module.exports = function(grunt) {
 
     for(var key in input_json) {
       if(input_json.hasOwnProperty(key)) {
+        if(input_json[key].substr(0,1) != "/") {
+          input_json[key] = "/" + input_json[key];
+        }
+        if(input_json[key].substr(-1,1) != "/") {
+          input_json[key] = input_json[key] + "/";
+        }
         files_sync_config.push({
           action: 'upload',
           expand: true,
